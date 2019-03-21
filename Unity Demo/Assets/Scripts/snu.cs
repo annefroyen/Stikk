c85f9bb5835a476b054d59aa16370cf2c81bca8e
h182025 <annehjefro@gmail.com>
2019-03-21 14:01:26 +0100

    snu
    lagde ein snu scene for å teste ut rotering og risting av mobil

---
+++ Unity Demo/Assets/Scripts/snu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class snu : MonoBehaviour
+{
+
+    Vector3 accelerationDir;
+    public Text antallTekst;
+    public int antall;
+
+    private void Start()
+    {
+        antall = 0;
+        SetAntallTekst("antall snu: ");
+        //tall = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        accelerationDir = Input.acceleration;
+
+        if (antall > 1)
+        {
+            antall = antall + 1;
+            SetAntallTekst("Antall snu: " + antall);
+        }
+
+        if(accelerationDir.sqrMagnitude >= 10f)
+        {
+            SetAntallTekst("Du ristet for hardt og ødela prøven!");
+
+        }
+       // transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+        
+    }
+
+    void SetAntallTekst(string i)
+    {
+        antallTekst.text = i;
+    }
+}
