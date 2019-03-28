97d294dc06cfc436e02f7373df6227aa120af981
h182025 <annehjefro@gmail.com>
2019-03-28 20:16:31 +0100

    Lagde ny test til vene
    Den skal vibrere, men det funker ikkje på app'en så no lager den bere ein lyd av hjerteslag

---
+++ Unity Demo/Assets/Scripts/vibrer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class vibrer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void viber()
+    {
+        Handheld.Vibrate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
