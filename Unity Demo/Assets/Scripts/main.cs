31c763c1695532b45afa11344998f82518da8c3c
h182025 <annehjefro@gmail.com>
2019-04-08 10:29:45 +0200

    jobbet med main
    Prøvde skrive script for å vise video

--- Unity Demo/Assets/Scripts/main.cs
+++ Unity Demo/Assets/Scripts/main.cs
@@ -1,18 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class main : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button kanyleKnapp;
+    public Button staseKnapp;
+    
+    public bool stase;
+    public bool kanyle;
+    public bool proveror1;
+    public bool proveror2;
+    public bool proveror3;
+
+
+    private VideoPlayer videoPlayer;
+    public MovieTexture MovieTexture;
+
     void Start()
     {
-        
+        kanyle = false;
+        stase = false;
+        videoPlayer = GetComponent<VideoPlayer>();
+       kanyleKnapp.onClick.AddListener(TaskOnClick);
+       staseKnapp.onClick.AddListener(TaskOnClick);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<Renderer>().material.mainTexture = MovieTexture;
+        if (Input.GetButton("kanyle"))
+        {
+            //kanyle er tyykt
+            kanyle = true;
+        }
+       
     }
+
+
+void TaskOnClick()
+{
+        //spill video
+        if (kanyle)
+        {
+            MovieTexture.Play();
+            videoPlayer.Play();
+            //spill video
+            if (kanyle)
+            {
+                //spill video
+            }
+        }
+   
+}
+
+
+
+
 }
